e36801e0252fd5c1aac07ef22fb1471d5bf4da87
Matt Schmulen <matt.schmulen@gmail.com>
2014-05-27 14:57:08 -0700

    build script update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 # com.mycompany.myapp - is app bundle identifier
 
-/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool --help
+# /Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool --help
 
-# /Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:Release|iPhone" "Path/To/YourSolution.sln" 
+echo "build the iOS library (can also be build from Xamarin Studio)"
 
-# /Applications/MonoDevelop.app/Contents/MacOS/mdtool -v build "--configuration:Release|iPhone" "Path/To/YourSolution.sln"
+/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:Release|iPhone" "../Crittercism.iOS/Crittercism.iOS.sln"
 
+echo "build the android library (can also be build from Xamarin Studio) "
 
+/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:Release|Android" "../Crittercism.iOS/Android.iOS.sln"
+
+echo "copy dll's to the component/bin folder"
+
+#iOS Libraries
+cp ../Crittercism.iOS/CrittercismXam/bin/Release/CrittercismXam.dll ../component/bin
+cp ../Crittercism.iOS/CrittercismXam/bin/Release/Crittercism.dll ../component/bin
+
+#Android Libraries
+cp ../Crittercism.Android/Crittercism.Android/bin/Release/Crittercism.Android.dll ../component/bin/
+
+echo "done, run '../component/rake' to build the component"
 
