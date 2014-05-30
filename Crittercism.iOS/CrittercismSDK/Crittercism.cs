56eb66cd5a43f5a2f0a5eba67fdafcd7284effbb
David Shirley <dshirley@crittercism.com>
2014-05-30 15:23:52 -0700

    Adding C# extensions to the binding library
    Per recommendation of http://developer.xamarin.com/guides/ios/advanced_topics/binding_objective-c/binding_objc_libs/
    
    We really do not need two libraries.
    Classes do NOT get removed from the binding library.
    We just thought that because we were adding classes to a file
    that gets compiled as ObjcBindingApiDefinition.
    
    This file should serve as the real interface (not the secondary middleman project)

---
+++ Crittercism.iOS/CrittercismSDK/Crittercism.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using MonoTouch.Foundation;
+
+namespace CrittercismSDK
+{
+
+	public partial class Crittercism
+	{
+		public static void Init(string appId) {
+			Crittercism._EnableWithAppID (appId);
+
+			AppDomain.CurrentDomain.UnhandledException += (sender, args) => {
+
+				Console.WriteLine("LogUnhandledException --UnhandledException :");
+				Console.Out.Flush();
+
+//				Console.WriteLine (args.ExceptionObject.GetType ());
+				//				CrittercismSDK.Crittercism.LogUnhandledException( 
+				//					args.ExceptionObject.GetType().ToString(),
+				//					args.ExceptionObject.GetType().ToString(),
+				//					Environment.StackTrace.ToString());
+			};
+				
+		}
+
+		static public void LogHandledException (System.Exception e)
+		{
+			CRCSharpException ex = new CRCSharpException(e.Message, e.Message, e.StackTrace, 0, 1);
+			Crittercism._LogCSharpException (ex);
+		}
+			
+	}
+}
+
