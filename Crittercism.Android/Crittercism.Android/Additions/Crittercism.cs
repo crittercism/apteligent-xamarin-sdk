5770a4f7f7ebecd63f260b943bd1ac17f5aae8df
Matt Schmulen <matt.schmulen@gmail.com>
2014-06-13 07:59:16 -0700

    android update

--- Crittercism.Android/Crittercism.Android/Additions/Crittercism.cs
+++ Crittercism.Android/Crittercism.Android/Additions/Crittercism.cs
@@ -34,7 +34,7 @@ namespace Crittercism.Android
 			//Console.WriteLine (" LogUnHandledException " + e.Exception.ToString());
 			Java.Lang.Exception javaLangException = new Java.Lang.Exception();
 			javaLangException.FillInStackTrace ();
-			Crittercism.LogCrashException (javaLangException);
+			LogCrashException (javaLangException);
 		}
 
 		public static void LogCrashException (Java.Lang.Exception javaException)
@@ -46,7 +46,7 @@ namespace Crittercism.Android
 		public static void LeaveBreadcrumb (string breadcrumb)
 		{
 			Console.WriteLine (" Crittercism.LeaveBreadcrumb " + breadcrumb);
-			//Com.Crittercism.App.Crittercism._logCrashException( e);
+			//Com.Crittercism.App.Crittercism._logCrashException();
 		}
 
 		public static void SetMetadata (string key, string value)
@@ -63,91 +63,22 @@ namespace Crittercism.Android
 			//Com.Crittercism.App.Crittercism.setUsername( username );
 		}
 		
-		public static bool DidCrashOnLastLoad
+		public static bool DidCrashOnLastLoad()
 		{
-			get { return true; /*Com.Crittercism.App.Crittercism.CrashedOnLastLoad;*/ }
+			return Com.Crittercism.App.Crittercism.DidCrashOnLastLoad(); 
 		}
 
-		public static bool GetOptOutStatus()
+		public static bool GGetOptOutStatus()
 		{
-			/*
-			ICritterCallback cb = new MyCritterCB ();
-			CritterUserDataRequest request = new CritterUserDataRequest (cb);
-			request.RequestOptOutStatus ();
-			*/
-			return false;
+			return Com.Crittercism.App.Crittercism.OptOutStatus;
 		}
 
-		/* 
-
-As a hack for now im implementing the exception handlers for android as per this blog post then calling Crittercism.LogHandledException(Throwable.FromException(e.Exception)); from inside both the events.
-
-It works, but im using Crittercism's handled exceptions for unhandled exceptions.. So when I want to send up real "HandledExceptions" they will be lost in the mess.
-
-
-
-		 */
-
-		/*
-		public boolean getOptOutStatus() {
-
-			final ConditionVariable condVar = new ConditionVariable(false);
-
-
-			CritterCallback cb = new CritterCallback() {
-
-
-				@Override public void onCritterDataReceived(CritterUserData userData) {
-
-					isOptedOut = userData.isOptedOut();
-
-					condVar.open();
-
-				}
-
-			};
-
-
-			// Instantiate data request object, and specify that it should include
-
-			// information on whether the has opted out.
-
-			CritterUserDataRequest request = new CritterUserDataRequest(cb)
-
-				.requestOptOutStatus()
-
-				.requestDidCrashOnLastLoad();
-
-
-
-			request.makeRequest();
-
-			condVar.block();
-
-
-
-			return isOptedOut;
-
-
-
-			// EXPERIMENTAL MASTODO http://stackoverflow.com/questions/19507452/how-to-handle-monodroid-uncaught-exceptions-globally-and-prevent-application-fro
-			// 
-			/*
-
-And then on button click I added the following code to raise both types of exceptions:
-
-//foreground exception
-throw new NullReferenceException("test nre from ui thread.");
-//background exception
-ThreadPool.QueueUserWorkItem(unused =>
-{
-    throw new NullReferenceException("test nre from back thread.");
-});
-
-
+		public static bool OptOutStatus
+		{
+			get { return Com.Crittercism.App.Crittercism.OptOutStatus; }
+			set { Com.Crittercism.App.Crittercism.OptOutStatus = value; }
 		}
-		*/
-		
+
 
 	}
 }
