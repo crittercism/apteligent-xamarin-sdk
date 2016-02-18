27c06e36496b22364ea6c20a8da625da924a2baa
David Shirley <dshirley@crittercism.com>
2016-02-18 11:41:16 -0800

    Link against the correct iOS SDK

--- Crittercism.iOS/Crittercism.iOS/libCrittercism_v5_5_2.linkwith.cs
+++ Crittercism.iOS/Crittercism.iOS/libCrittercism_v5_5_2.linkwith.cs
@@ -1,7 +1,7 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libCrittercism_v5_4_2.a",
+[assembly: LinkWith ("libCrittercism_v5_5_2.a",
 	LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64,
 	SmartLink = true,
 	ForceLoad = true,
